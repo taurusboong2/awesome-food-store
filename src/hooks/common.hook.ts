b43233e05ba6c95031dd3471a7b82853aa82b422
boong <taurusboong2@gmail.com>
2023-02-05 00:00:42 +0900

    feat: redirect custom hook

---
+++ src/hooks/common.hook.ts
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export const useNotFoundRedirect = (delay: number) => {
+  const router = useRouter();
+
+  const [second, setSecond] = useState<number>(delay);
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, delay * 1000);
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSecond(second - 1);
+    }, 1000);
+
+    if (second === 0) {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [second]);
+
+  return { second };
+};
