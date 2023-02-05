20fe69b3134c696e83d6150931e5d34f4691c52a
boong <taurusboong2@gmail.com>
2023-02-05 21:22:54 +0900

    chore: import ordering

--- src/hooks/common.hook.ts
+++ src/hooks/common.hook.ts
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export const useNotFoundRedirect = (delay: number) => {
   const router = useRouter();
